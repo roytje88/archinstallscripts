b28eaf8aa40b28cbe26bf4df33c02d4c67b7bc9c
roytje88 <royvanrhedenburg@gmail.com>
2021-03-07 10:42:26 +0100

    root password

--- script.sh
+++ script.sh
@@ -42,7 +42,39 @@ do
 		userdel -r alarm
 		;;
 	2)
-		
+        while [ -z "$check" ]
+            do
+                password=$(dialog --passwordbox "Enter the desired root password" 10 30 --output-fd 1)
+                if [ ! $? -eq 255 ]; then
+                    confirmpw=$(dialog --passwordbox "confirm your password" 10 30 --output-fd 1)
+                else
+                    dialog --title "Information" --msgbox "Canceled! Root password unchanged!" 6 44
+                    break
+                fi
+                if [ ! $? -eq 255 ]; then
+                    if [ "$password" = "$confirmpw" ]; then
+                        if [ -z "$password" ]; then
+                            dialog --title "Information" --msgbox "Password cannot be empty! Please try again." 6 44
+                        else
+                            check="not empty"
+                        fi
+                    elif [ $? -eq 255]; then
+                        dialog --title "Information" --msgbox "Canceled! Root password unchanged!" 6 44
+                        break
+                    else
+                        dialog --title "Information" --msgbox "Passwords do not match! Please try again." 6 44
+                    fi
+                else
+                    dialog --title "Information" --msgbox "Canceled! Root password unchanged!" 6 44
+                    break
+                fi
+            done
+        if [ ! -z "$check" ]; then
+            echo $password | passwd --stdin
+            dialog --title "Information" --msgbox "Root password changed!" 6 44
+        fi
+        ;;
+        
     esac
 
 done
