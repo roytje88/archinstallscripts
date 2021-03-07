e80fc1db8f334b3f65ead3d765a9ebe8913864ae
roytje88 <royvanrhedenburg@gmail.com>
2021-03-07 10:48:56 +0100

    default user/password

--- script.sh
+++ script.sh
@@ -74,7 +74,43 @@ do
             dialog --title "Information" --msgbox "Root password changed!" 6 44
         fi
         ;;
-        
+    3)
+        username=$(dialog --passwordbox "Enter the desired username for the default user" 10 30 --output-fd 1)
+        if [ ! -z "$username" ]; then
+            useradd -m -g users -G wheel,storage,power -s /bin/bash $username
+        fi
+        while [ -z "$check" ]
+            do
+                password=$(dialog --passwordbox "Enter the desired password for $username" 10 30 --output-fd 1)
+                if [ ! $? -eq 255 ]; then
+                    confirmpw=$(dialog --passwordbox "confirm your password" 10 30 --output-fd 1)
+                else
+                    dialog --title "Information" --msgbox "Canceled! Password unchanged for $username!" 6 44
+                    break
+                fi
+                if [ ! $? -eq 255 ]; then
+                    if [ "$password" = "$confirmpw" ]; then
+                        if [ -z "$password" ]; then
+                            dialog --title "Information" --msgbox "Password cannot be empty! Please try again." 6 44
+                        else
+                            check="not empty"
+                        fi
+                    elif [ $? -eq 255]; then
+                        dialog --title "Information" --msgbox "Canceled! Password unchanged for $username!" 6 44
+                        break
+                    else
+                        dialog --title "Information" --msgbox "Passwords do not match! Please try again." 6 44
+                    fi
+                else
+                    dialog --title "Information" --msgbox "Canceled! Password unchanged for $username!" 6 44
+                    break
+                fi
+            done
+        if [ ! -z "$check" ]; then
+            echo $password | passwd --stdin
+            dialog --title "Information" --msgbox "Password changed for $username!" 6 44
+        fi
+        ;;
     esac
 
 done
--- tests/pass.sh
+++ tests/pass.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+username=$(dialog --passwordbox "Enter the desired username for the default user" 10 30 --output-fd 1)
 
+if [ ! -z "$username" ]; then
+    useradd -m -g users -G wheel,storage,power -s /bin/bash $username
 
+fi
 
     while [ -z "$check" ]
         do
-            password=$(dialog --passwordbox "Enter the desired root password" 10 30 --output-fd 1)
+            password=$(dialog --passwordbox "Enter the desired password for $username" 10 30 --output-fd 1)
             if [ ! $? -eq 255 ]; then
                 confirmpw=$(dialog --passwordbox "confirm your password" 10 30 --output-fd 1)
             else
-                dialog --title "Information" --msgbox "Canceled! Root password unchanged!" 6 44
+                dialog --title "Information" --msgbox "Canceled! Password unchanged for $username!" 6 44
                 break
             fi
             if [ ! $? -eq 255 ]; then
@@ -20,7 +24,7 @@
                         check="not empty"
                     fi
                 elif [ $? -eq 255]; then
-                    dialog --title "Information" --msgbox "Canceled! Root password unchanged!" 6 44
+                    dialog --title "Information" --msgbox "Canceled! Password unchanged for $username!" 6 44
                     break
                 else
                     dialog --title "Information" --msgbox "Passwords do not match! Please try again." 6 44
